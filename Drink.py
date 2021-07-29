2c2bb80ea8203204cd2765d8de01d73df40cf15c
jvanek <jvanek>
2021-07-28 23:30:26 -0400

    Add drink class

---
+++ Drink.py
@@ -0,0 +1,15 @@
+class Drink:
+    name = ""
+    source = ""
+    origin = ""
+    directions = ""
+    isMocktail = False
+    ingredients = []
+    
+    #Constructor
+    def __init__(self, name, source, origin, directions, isMocktail):
+        self.name = name
+        self.source = source
+        self.origin = origin
+        self.directions = origins
+        self.isMocktail = isMocktail
