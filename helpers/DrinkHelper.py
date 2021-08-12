478d5c15f98132d037c4b21c461598d709f224d6
John Vanek <bigj13232@gmail.com>
2021-08-11 23:52:26 -0400

    update

--- helpers/DrinkHelper.py
+++ helpers/DrinkHelper.py
@@ -1,3 +1,4 @@
+from helpers.MenuHelper import MenuHelper
 import sqlite3
 from sqlite3 import Error
 
@@ -14,7 +15,56 @@ class DrinkHelper(object):
         return conn
 
     @staticmethod
+    def create_table(conn, create_table_sql):
+        #   create table from the create_table_sql statement
+        #   :param conn: Connecion object
+        #   :param create_table_sql: a CREATE TABLE statement
+        #   :return: none
+        try:
+            c = conn.cursor()
+            c.execute(create_table_sql)
+        except Error as e:
+            print(e)
+
+    @staticmethod
     def addDrink():
+        drinkLoop = 'X'
+        while drinkLoop != 'Y':
+            name = input("Enter drink name: ")
+            MenuHelper.displaySources()
+            sourceChoice = int(-1)
+
+            while(sourceChoice <=0 or sourceChoice > 8):
+                sourceChoice = int(input("Enter drink source: "))
+                if sourceChoice == 1: source = "Anime"
+                elif sourceChoice == 2: source = "Books"
+                elif sourceChoice == 3: source = "Cartoons"
+                elif sourceChoice == 4: source = "Comic Books"
+                elif sourceChoice == 5: source = "Games"
+                elif sourceChoice == 6: source = "Movies"
+                elif sourceChoice == 7: source = "TV"
+                elif sourceChoice == 8: source = "Video Games"
+                else: print("Invalid Choice - Try again.")
+
+            origin = str(input("Enter drink origin: "))
+            directions = str(input("Enter drink directions: "))
+
+            mocktailAnswer = str(input("Is the drink a mocktail? Yes/No"))
+            mocktailAnswer = mocktailAnswer.upper
+            mocktailAnswer2 = mocktailAnswer[0]
+            mocktail = 'X'
+            if mocktailAnswer2 == 'T': mocktail = int(1)
+            elif mocktailAnswer2 == 'F': mocktail = int(0)
+
+            #   Confirm drink details
+            print("Entered drink name:   " + name)
+            print("Entered drink source: " + source)
+            print("Entered drink origin: " + origin)
+            print("Entered mocktail:     " + mocktail)
+            print("Entered drink directions: \r" + directions + "\n")
+
+            drinkLoop = str(input("is the above correct?"))
+
         DrinkHelper.create_connection(r"drink.db")
         print("Add Drink")
     
